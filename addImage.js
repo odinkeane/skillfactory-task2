3c59e70121225202b6d4b1bcc7f7b1e2aa181de4
odinkeane <138715688+odinkeane@users.noreply.github.com>
2023-11-26 01:05:10 +0700

    Add files via upload

--- addImage.js
+++ addImage.js
@@ -1,6 +1,6 @@
 let mini_articles = document.getElementsByClassName("main__small-articles__article__image");
 for (let i = 0; i < mini_articles.length; i++) {
-    mini_articles[i].style.backgroundImage = `url(../images/blog-images/blog-images-mini/small_${i+1}.png)`;
+    mini_articles[i].style.backgroundImage = `url(images/blog-images/blog-images-mini/small_${i+1}.png)`;
     mini_articles[i].style.backgroundSize = 'cover';
     mini_articles[i].style.backgroundPosition = 'center';
 
